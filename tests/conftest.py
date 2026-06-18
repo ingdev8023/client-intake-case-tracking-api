b4ed5301507a64414c25ca0ebfcd8e9b9ac36469
ingdev8023 <ingdev8023@gmail.com>
2026-06-17 21:00:03 -0500

    added: 3 pytest fixtures, 1 for the admin user, 1 for the staff user and 1 for an inactive user, added: 2 tests for the admin role and the inactive user

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from app import create_app
-from app.extensions.extensions import db
-
+from app.extensions.extensions import db, bcrypt
+from app.models.models import User
 
 @pytest.fixture()
 def app():
@@ -21,3 +21,55 @@ def app():
 @pytest.fixture()
 def client(app):
     return app.test_client()
+
+@pytest.fixture()
+def admin_user(app):
+    hashed_password = bcrypt.generate_password_hash("Password123!").decode("utf-8")
+
+    user = User(
+        user_name="Admin User",
+        user_email="admin@test.com",
+        user_password=hashed_password,
+        user_role="admin",
+        is_active=True,
+    )
+
+    db.session.add(user)
+    db.session.commit()
+
+    return user
+
+@pytest.fixture()
+def staff_user(app):
+    hashed_password = bcrypt.generate_password_hash("Password123!").decode("utf-8")
+
+    user = User(
+        user_name="Staff User",
+        user_email="staff@test.com",
+        user_password=hashed_password,
+        user_role="staff",
+        is_active=True,
+    )
+
+    db.session.add(user)
+    db.session.commit()
+
+    return user
+
+
+@pytest.fixture()
+def inactive_user(app):
+    hashed_password = bcrypt.generate_password_hash("Password123!").decode("utf-8")
+
+    user = User(
+        user_name="Inactive User",
+        user_email="inactive@test.com",
+        user_password=hashed_password,
+        user_role="staff",
+        is_active=False,
+    )
+
+    db.session.add(user)
+    db.session.commit()
+
+    return user
--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -6,3 +6,23 @@ def test_cases_requires_token(client):
     response = client.get("/cases")
 
     assert response.status_code == 401
+
+def test_valid_login_returns_token(client, admin_user):
+    response = client.post("/login", json={
+        "user_email": "admin@test.com",
+        "user_password": "Password123!"
+    })
+
+    data = response.get_json()
+
+    assert response.status_code == 200
+    assert "access_token" in data
+
+def test_inactive_user_cannot_login(client, inactive_user):
+    response = client.post("/login", json={
+        "user_email": "inactive@test.com",
+        "user_password": "Password123!"
+    })
+
+    assert response.status_code == 403
+    
